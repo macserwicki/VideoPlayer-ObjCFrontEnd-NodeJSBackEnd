da8cf5349826ec1cfb80868978afc557f3a4d2f2
Maciej Serwicki <macserwicki@gmail.com>
2016-05-14 11:26:58 -0700

    Video Webview Resizes WUsing CSS and Javascript

--- VideoPlayer/VideoViewController.m
+++ VideoPlayer/VideoViewController.m
@@ -24,11 +24,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+    self.webView.delegate = self;
     self.titleLabel.text = self.video.videoTitle;
     self.descriptionLabel.text = self.video.videoDescription;
     [self.webView loadHTMLString:self.video.videoIframe baseURL:nil];
     
+    
+}
+
+//http://stackoverflow.com/questions/6903292/uiwebview-css-injection-using-javascript
+-(void)webViewDidFinishLoad:(UIWebView*)webView{
+    NSString *css = @".container {position: relative;width: 100%;height: 0;padding-bottom: 56.25%;}.video {position: absolute;top: 0;left: 0;width: 100%;height: 100%;}";
+    NSString *js = [NSString stringWithFormat:
+                    @"var styleNode = document.createElement('style');\n"
+                    "styleNode.type = \"text/css\";\n"
+                    "var styleText = document.createTextNode('%@');\n"
+                    "styleNode.appendChild(styleText);\n"
+                    "document.getElementsByTagName('head')[0].appendChild(styleNode);\n",css];
+    
+    [self.webView stringByEvaluatingJavaScriptFromString:js];
 }
 
 - (void)didReceiveMemoryWarning {
